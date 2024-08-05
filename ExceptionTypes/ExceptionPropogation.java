29094e5a66d4ef07a45d9890b0f9c1d6b97d589b
Ajaygattu781 <Ajay.Gattu@godigit.com>
2024-08-05 14:29:03 +0530

    ExceptionPropogation Type

--- ExceptionTypes/ExceptionPropogation.java
+++ ExceptionTypes/ExceptionPropogation.java
@@ -1,2 +1,20 @@
-package ExceptionTypes;public class ExceptionPropogation {
+package ExceptionTypes;
+
+public class ExceptionPropogation {
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+        }
+    }
+    public static void method1() throws Exception {
+        method2();
+    }
+    public static void method2() throws Exception {
+        method3();
+    }
+    public static void method3() throws Exception {
+        throw new Exception("Exception in method3");
+    }
 }
