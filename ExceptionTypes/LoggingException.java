d1eaebd6cf7d0b9bff9997fe0a34be6b81910046
Ajaygattu781 <Ajay.Gattu@godigit.com>
2024-08-05 14:29:38 +0530

    Exception Logging Type

--- ExceptionTypes/LoggingException.java
+++ ExceptionTypes/LoggingException.java
@@ -1,2 +1,18 @@
-package ExceptionTypes;public class LoggingException {
+package ExceptionTypes;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class LoggingException {
+    private static final Logger LOGGER =
+            Logger.getLogger(LoggingException.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            throw new Exception("Sample exception");
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Exception occurred",
+                    e);
+        }
+    }
 }
+
