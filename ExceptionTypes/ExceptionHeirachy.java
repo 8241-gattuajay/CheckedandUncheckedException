dd95ac3bc668eaec6d5365ec2bd613c5677ddc7a
Ajaygattu781 <Ajay.Gattu@godigit.com>
2024-08-05 14:28:22 +0530

    ExceptionHeirachy Exception Type

--- ExceptionTypes/ExceptionHeirachy.java
+++ ExceptionTypes/ExceptionHeirachy.java
@@ -1,2 +1,27 @@
-package ExceptionTypes;public class ExceptionHeirachy {
+package ExceptionTypes;
+
+class ApplicationException extends Exception {
+    public ApplicationException(String message) {
+        super(message);
+    }
 }
+class DatabaseException extends ApplicationException {
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
+class NetworkException extends ApplicationException {
+    public NetworkException(String message) {
+        super(message);
+    }
+}
+public class ExceptionHeirachy {
+    public static void main(String[] args) {
+        try {
+            throw new DatabaseException("Database error");
+        } catch (ApplicationException e) {
+            System.out.println("Caught: " + e);
+        }
+    }
+}
+
