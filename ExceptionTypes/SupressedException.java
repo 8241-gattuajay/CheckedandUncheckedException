c6cc6f5f2401ca55c6f4a51ab1c03833490ea269
Ajaygattu781 <Ajay.Gattu@godigit.com>
2024-08-05 14:30:11 +0530

    Exception Suppressed Type

--- ExceptionTypes/SupressedException.java
+++ ExceptionTypes/SupressedException.java
@@ -1,2 +1,23 @@
-package ExceptionTypes;public class SupressedException {
+package ExceptionTypes;
+
+public class SupressedException {
+    public static void main(String[] args) {
+        try (Resource resource = new Resource()) {
+            resource.doSomething();
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            for (Throwable suppressed : e.getSuppressed()) {
+                System.out.println("Suppressed: " + suppressed);
+            }
+        }
+    }
+}
+class Resource implements AutoCloseable {
+    @Override
+    public void close() throws Exception {
+        throw new Exception("Exception in close");
+    }
+    public void doSomething() throws Exception {
+        throw new Exception("Exception in doSomething");
+    }
 }
